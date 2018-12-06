f880f2d392159c6252cd561a067fbebd0e5bc569
Carlos Mañas <carlos@spacenomads.com>
2018-12-06 16:55:35 +0100

    Update js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,28 @@
 const field = document.querySelector('.app__field');
 const searchBTN = document.querySelector('.app__search-btn');
 const nameContainer = document.querySelector('.name');
-const apiPath = 'https://api.github.com/users/';
 
-const getUserInfo = () => {
-  const user = field.value;
-  const url = apiPath + user;
-  
+const api = 'https://api.github.com/users/';
+
+
+const getUserName = () => {
+  const nick = field.value;
+  const url = api + nick;
+
   fetch(url)
     .then(r=>r.json())
     .then(data=>{
-      const fullName = data.name; // Carlos Mañas
-      const arrName = fullName.split(' '); // ['Carlos', 'Mañas']
-      const userName = arrName[0]; // 'Carlos'
+      const fullName = data.name.split(' '); // ["Carlos", "Mañas"]
+      const userName = fullName[0]; // "Carlos"
 
-      let items = '';
-      
-      for (let i=0; i<userName.length;i++) {
-        items += `<li class="name__item">${userName[i]}</li>`;
+      let names = '';
+      for (let i=0;i<userName.length;i++) {
+        names += `<li class="name__item">${userName[i]}</li>`;
       }
-      nameContainer.innerHTML = items;
+      nameContainer.innerHTML = names;
     });
-
+  
 };
 
-searchBTN.addEventListener('click', getUserInfo);
+
+searchBTN.addEventListener('click', getUserName);
